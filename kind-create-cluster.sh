7804a21eec9a0d44d1687e5f4ef299bb109d9e5f
Jan Pazdziora <jan.pazdziora@kube.adelton.com>
2023-12-06 19:02:44 +0100

    Control the cluster config location with the KIND_CLUSTER_CONFIG variable.

--- kind-create-cluster.sh
+++ kind-create-cluster.sh
@@ -2,10 +2,18 @@
 
 set -e
 
-if podman ps -a --noheading | grep . ; then
+OPTS=
+if [ -n "$KIND_CLUSTER_CONFIG" -a -s "$KIND_CLUSTER_CONFIG" ] ; then
+	( set -x ; cp $KIND_CLUSTER_CONFIG /var/lib/containers/kind-cluster.yaml )
+	OPTS="--config /var/lib/containers/kind-cluster.yaml"
+fi
+
+if podman ps -a --noheading | grep -q . ; then
+	set -x
+	podman ps -a
 	exit
 fi
 
 set -x
-kind create cluster "$@"
+kind create cluster $OPTS "$@"
 
